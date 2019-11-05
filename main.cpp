bad79dec5584fc2d89cb773f136de145443d13e3
ortlaz <anastasea.com@gmail.com>
2019-11-05 17:53:58 +0300

    Сделал Менюшку

--- main.cpp
+++ main.cpp
@@ -11,48 +11,56 @@ int main()
     SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+    int var;
 
-	//TODO: доделать меню
-        //  исправить костыль
-        //  c отрицательными числа
-        //  и одинарными
-		/*while(1) {
-			try {
-		       char buf[50];
-		       string str_in;
-		       cout << "Введите выражение" << endl;
+    while (var){
+        system("cls");
+        cout<<"Меню:\n1.Стек на основе массиве.\n2.Стек на основе списка\n0.Выход\n";
+        cout<<"Выберите опцию:";
+        cin>>var;
+
+        switch(var){
+            case 1:{
+                char buf[50];
+                string str_in;
+                cout << "Введите выражение" << endl;
                 cin >> buf;                          //get the input string
-			   if(buf[0] == '0') break;
-		       str_in = buf;
-               Opz ppn;
-		       ppn.convert(str_in);
-		       cout << '\n' << ppn.getOutputStr() << '\n' << endl;
-		       cout<<ppn.calculate(ppn.getOutputStr())<<endl;
+                if(buf[0] == '0') break;
+                str_in = buf;
 
+                Opz ppn;
+                ppn.convert(str_in);
+                cout << "Обратная польска запись:" << ppn.getOutputStr()<< endl;
+                cout<<"Результат:"<<ppn.calculate(ppn.getOutputStr())<<endl;
+                system("pause");
+                break;
 			}
-			catch(string exc) {
-		       cout << exc << '\n' << endl;
-			}
-		}
-*/
-        while(1) {
-			try {
-		       char buf[50];
-		       string str_in;
-		       cout << "Введите выражение" << endl;
-                cin >> buf;                          //get the input string
-			   if(buf[0] == '0') break;
-		       str_in = buf;
-               Ppn ppn;
-		       ppn.convert(str_in);
-		       cout << '\n' << ppn.getOutputStr() << '\n' << endl;
-		       cout<<ppn.calculate(ppn.getOutputStr())<<endl;
+			case 2:{
+                char buf1[50];
+                string strin;
+                cout << "Введите выражение" << endl;
+                cin >> buf1;                          //get the input string
+                if(buf1[0] == '0') break;
+                strin = buf1;
 
+                Ppn ppn;
+                ppn.convert(strin);
+                cout << "Обратная польска запись:" << ppn.getOutputStr()<< endl;
+                cout<<"Результат:"<<ppn.calculate(ppn.getOutputStr())<<endl;
+                system("pause");
+			    break;
 			}
-			catch(string exc) {
-		       cout << exc << '\n' << endl;
+			case 0:{
+                exit(0);
+                system("pause");
+                break;
 			}
-		}
+            default:{
+            cout<<"Выберите опцию из списка!\n";
+            system("pause");
+            }
+        }
+    }
 
     return 0;
 }
